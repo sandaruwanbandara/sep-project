293fd3e80f039338ddf937e00aed397b16f84b2a
sandaruwan <sandaruwanbest@gmail.com>
2022-02-24 19:59:37 +0530

    profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -27,13 +29,19 @@ class ProfileController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
+    public function update(Request $request,User $user)
     {
         $request->validate([
             'name' => 'required',
             'about' => 'required',
-            'contact' => 'required',
+            'contact' => 'required|regex:/(0)[0-9]{9}/',
         ]);
+
+        $user_params = ["name"=>$request->name,"about"=>$request->about,"contact"=>$request->contact];
+        $logged_user = Auth::user();
+
+        $user->where('email',$logged_user->email)->update($user_params);
+
         return redirect()->route('profile');
     }
 
@@ -43,12 +51,20 @@ class ProfileController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function passwordUpdate(Request $request)
+    public function passwordUpdate(Request $request,User $user)
     {
         $request->validate([
             'new_password' => ['required', 'confirmed', Password::min(8)->mixedCase()],
             'password' => ['required'],
         ]);
+
+        $logged_user = Auth::user();
+        $upwd_params = bcrypt($request->new_password);
+
+        if (Hash::check($request->password, $logged_user->getAuthPassword())) {
+            $user->where('email',$logged_user->email)->update(['password'=>$upwd_params]);
+        }
+
         return redirect()->route('profile');
     }
 
