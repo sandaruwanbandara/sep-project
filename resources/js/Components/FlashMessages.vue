7d86d636bbc5cce131c5ce1b58abe4315a280d97
dularadilshan <ms22910790@my.sliit.lk>
2022-02-24 13:15:15 +0530

    Profile updating functionalities implemented and UI updated

---
+++ resources/js/Components/FlashMessages.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div v-if="$page.props.flash.message" class="max-w-3xl mt-2 mx-auto sm:px-6 lg:px-8 bg-green-100 border-l-4 border-green-500 text-green-700 p-4" role="alert">
+        <p> {{ $page.props.flash.message }} </p>
+    </div>
+</template>
